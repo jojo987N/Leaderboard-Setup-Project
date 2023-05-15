1cadc2170c67070e8ace902ed94de079636f08df
jemba narcisse <cabliveer@gmail.com>
2023-05-15 17:42:42 +0200

    Solver linter errors

--- src/modules/score.js
+++ src/modules/score.js
@@ -1,13 +1,12 @@
 export default class Score {
- 
-    constructor(name, score){
-        this.name = name
-        this.score = score;
-    }
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
 
-    render(i){
-        return `
-        <li class="${i%2 === 0 || "bg-grey"}">${this.name}: ${this.score}</li>
-        `
-    }
+  render(i) {
+    return `
+        <li class="${i % 2 === 0 || 'bg-grey'}">${this.name}: ${this.score}</li>
+        `;
+  }
 }
