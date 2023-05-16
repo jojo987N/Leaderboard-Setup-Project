26ebc55b68e63c54c32b53341c12f5305ee3de7e
jemba narcisse <cabliveer@gmail.com>
2023-05-16 17:59:47 +0200

    import all modules follow ES6

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import AddScore from './modules/addScore.js';
+import Game from './modules/game.js';
 import List from './modules/list.js';
 import RecentScores from './modules/recentScores.js';
 import Score from './modules/score.js';
+import Store from './modules/store.js';
 import './style.scss';
 
-const list = new List();
+export const URL_BASE = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-[100, 20, 50, 78, 125, 77, 42].forEach((score) => {
-  list.add(new Score('Name', score));
-});
+(async () => {
+  const game = localStorage.getItem('game') || (await (await Game.init('Pokemon')).json()).result.slice(14, 34);
+  localStorage.setItem('game', game);
 
-document.querySelector('.content-1').innerHTML = new RecentScores(list).render();
+  document.querySelector('.submit').onclick = (e) => {
+    e.preventDefault();
+    AddScore.add(new Score(), game);
+  };
+  document.querySelector('.refresh').onclick = async () => {
+    window.location.reload();
+  };
+  RecentScores.render((await (await Store.get(game)).json()).result, List);
+})();
